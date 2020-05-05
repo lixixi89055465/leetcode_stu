1ab1493aa678058df2ad922fc3488eb664588532
nanji <1850094299@qq.com>
2020-05-05 21:20:45 +0800

    init

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
 
 int main() {
-    printf("aaaa");
+    short x = -4321;
+    unsigned short y = (unsigned short) x;
+    int a = 165537, b = -34991;
+    short c = (short) a, d = (short) b;
+    short e = 1;
 }
