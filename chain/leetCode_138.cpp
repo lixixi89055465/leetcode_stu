d27b27b394c4f309f6d3f617d1ccf0272fde3513
nanji <1850094299@qq.com>
2020-05-30 19:40:47 +0800

    leetcode_138

---
+++ chain/leetCode_138.cpp
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#include "map"
+
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+
+int main() {
+    std::map<RandomListNode *, int> node_map;
+    RandomListNode a(5);
+    RandomListNode b(3);
+    RandomListNode c(6);
+    a.next = &b;
+    b.next = &c;
+    a.random = &c;
+    b.random = &a;
+    c.random = &c;
+    node_map[&a] = 1;
+    node_map[&b] = 2;
+    node_map[&c] = 3;
+    printf("a.random id = %d\n", node_map[a.random]);
+    printf("b.random id = %d \n ", node_map[b.random])
+    printf("c.random id = %d \n", node_map[c.random]);
+    return 0;
+}
