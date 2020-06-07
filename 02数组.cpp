7da7415b61463d0d40621b2a3c4ccd00deab8777
1850094299@qq.com <1850094299@qq.com>
2020-06-07 15:31:27 +0800

    138

--- 02数组.cpp
+++ 02数组.cpp
@@ -6,5 +6,9 @@ int main() {
 	cout << "整个数组占用的内存大小:"<<sizeof(arr) << endl;
 	cout << "统计"<<sizeof(arr[0]) << endl;
 	cout << "地址为:" << arr << endl;
+	int arrs[2][3] = { {1,2,3},{4,5,6} };
+	cout << arrs << endl;
+	cout << arrs[0] << endl;
+	cout << &arrs[0][0] << endl;
 	return 0;
 }
