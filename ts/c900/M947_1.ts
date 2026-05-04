5ebae53d14ff8253fda88c1c0903742eaf1cc1e3
lixixi89055465 <nanji1850094299@outlook.com>
2026-05-04 17:25:42 +0800

    init

--- ts/c900/M947_1.ts
+++ ts/c900/M947_1.ts
@@ -3,21 +3,13 @@ function removeStones(stones: [number, number][]): number {
         new Map<number, Set<[row: number, col: number]>>()
     const parent_col =
         new Map<number, Set<[row: number, col: number]>>()
-
-    function merge(ar: [row: number, col: number],
-                   rp: Set<[row: number, col: number]>) {
-        parent_row.set(ar[0], rp)
-        parent_col.set(ar[1], rp)
-        rp.add(ar)
-    }
-
     for (const stone of stones) {
         const [row, col] = stone
         const rp = parent_row.get(row)
         const cp = parent_col.get(col)
         if ((parent_row.has(row) || parent_col.has(col)) && rp || cp) {
             if (rp === cp && cp && rp) {
-                merge(stone, rp)
+                merge(stone, cp)
             } else {
                 combine(stone, rp, cp)
             }
@@ -28,23 +20,31 @@ function removeStones(stones: [number, number][]): number {
 
     function create(stone: [number, number], row: number, col: number) {
         const set = new Set([stone])
-        parent_row.set(row, set)
-        parent_col.set(col, set)
+        parent_row.set(col, set)
+        parent_col.set(row, set)
+    }
+
+    function merge(ar: [row: number, col: number],
+                   rp: Set<[row: number, col: number]>) {
+        parent_row.set(ar[0], rp)
+        parent_col.set(ar[1], rp)
+        rp.add(ar)
     }
 
     function combine(stone: [number, number],
                      rp: Set<[row: number, col: number]> | undefined,
                      cp: Set<[row: number, col: number]> | undefined) {
         const series: Set<[row: number, col: number]> = rp || cp || new Set
-
         cp?.forEach(s => series.add(s))
         rp?.forEach(s => series.add(s))
         merge(stone, series)
         series.forEach(([row, col]) => {
-            parent_col.set(col, series)
             parent_row.set(row, series)
+            parent_col.set(col, series)
         })
     }
 
-    return stones.length - new Set([...parent_col.values(), ...parent_row.values()]).size
+    return stones.length -
+        new Set([...parent_col.values(), ...parent_row.values()]).size
+
 }
